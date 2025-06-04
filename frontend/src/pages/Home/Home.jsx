f6702d5c743611ef4afde01a4a5262a17a1bb3de
Samolazoff <samolazov.roman.alex@gmail.com>
2025-06-04 16:41:32 +0300

    Add blocks mini in Home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import {NavLink} from 'react-router';
+import {NavLink, Link} from 'react-router';
 
 import page from '/ChatGPT.png';
 
@@ -7,20 +7,38 @@ import './Home.scss';
 const Home = () => {
     return (
         <section className='container home-page'>
-            <div className="home-page-txt-box">
-                <h2 className="home-page__title">
-                    Hi, I'm Raman Samalazau
-                </h2>
-                <span className="home-page__subtitle">Web Developer</span>
-                <span className="home-page__txt">
-                    I  specialize in building and designing web applications.
-                </span>
-                <div className="home-page-btn-box">
-                    <NavLink className='btn btn__main' to={'/about'}> View My Skills</NavLink>
-                    <NavLink className='btn btn__important' to={'/projects'}>View My Work</NavLink>
+            <div className="home-page-start-box">
+                <div className="home-page-txt-box">
+                    <h2 className="title-block">
+                        Hi, I'm Raman Samalazau
+                    </h2>
+                    <span className="home-page__subtitle">Web Developer</span>
+                    <span className="home-page__txt">
+                        I  specialize in building and designing web applications.
+                    </span>
+                    <div className="home-page-btn-box">
+                        <NavLink className='btn btn__main' to={'/about'}> View My Skills</NavLink>
+                        <NavLink className='btn btn__important' to={'/projects'}>View My Work</NavLink>
+                    </div>
                 </div>
+                <img src={page} alt="home-page__img" className="home-page__img" />
             </div>
-            <img src={page} alt="home-page__img" className="home-page__img" />
+            <aside className='mini-about'>
+                <Link to='/about'>
+                    <h2 className="title-block">About Me</h2>
+                </Link>
+                 <span className="home-page__txt">
+                    I  am a web developer with a passion for creating beautiful, resposive and user-friendly websites. I have experience in developing web applications using modern technologies. 
+                </span>
+            </aside>
+            <article className='mini-projects'>
+                <Link to='/projects'>
+                    <h2 className="title-block">Projects</h2>
+                </Link>
+                <span className="home-page__txt">
+                    A brief description of projects.
+                </span>
+            </article>
         </section>
     );
 };
