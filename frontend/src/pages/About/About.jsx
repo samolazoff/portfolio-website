e6e75cd6a3bedf0d6e375526b0abd513a6f2edaa
Samolazoff <samolazov.roman.alex@gmail.com>
2025-06-08 08:07:10 +0300

    Process create about page (create skills)

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -2,8 +2,89 @@ import './About.scss';
 
 import page from '/ChatGPT.png';
 
-const About = () => {
+import imgHtml from '../../static/icons/html-5.svg';
+import imgJs from '../../static/icons/js.svg';
+import imgCss from '../../static/icons/css-3-svgrepo-com.svg';
+import imgReact from '../../static/icons/react-svgrepo-com.svg';
+import imgNode from '../../static/icons/nodejs-svgrepo-com.svg';
+import imgRedux from '../../static/icons/redux-svgrepo-com.svg';
+import imgFigma from '../../static/icons/figma-svgrepo-com.svg';
+import imgSass from '../../static/icons/sass-svgrepo-com.svg';
+import imgPug from '../../static/icons/pug-svgrepo-com.svg';
+import imgNpm from '../../static/icons/npm-svgrepo-com.svg';
+import imgMongo from '../../static/icons/mongo-svgrepo-com.svg';
+import imgBoots from '../../static/icons/bootstrap-svgrepo-com.svg';
+import imgTail from '../../static/icons/tailwind-svgrepo-com.svg';
+import imgGit from '../../static/icons/git-svgrepo-com.svg';
 
+function itemStack(name, img) {
+    return(
+        <li className="about-page-skills__item" key={name}>
+            <img src={img} alt={name} title={name}/>
+        </li>
+    )
+};
+
+const imgArr = [
+        {
+            name: 'Html5',
+            img: imgHtml
+        },
+        {
+            name: 'CSS3',
+            img: imgCss
+        },
+        {
+            name: 'JS',
+            img: imgJs
+        },
+        {
+            name: 'ReactJs',
+            img: imgReact
+        },
+        {
+            name: 'NodeJs',
+            img: imgNode
+        },
+        {
+            name: 'Redux',
+            img: imgRedux
+        },
+        {
+            name: 'Figma',
+            img: imgFigma
+        },
+        {
+            name: 'Sass',
+            img: imgSass
+        },
+        {
+            name: 'Pug',
+            img: imgPug
+        },
+        {
+            name: 'npm',
+            img: imgNpm
+        },
+        {
+            name: 'MongoDB',
+            img: imgMongo
+        },
+        {
+            name: 'Bootstrap',
+            img: imgBoots
+        },
+        {
+            name: 'Tailwind',
+            img: imgTail
+        },
+        {
+            name: 'Git',
+            img: imgGit
+        },
+];
+
+const About = () => {
     return (
         <section className='about-page container'>
             <div className="about-page-start-box">
@@ -17,13 +98,25 @@ const About = () => {
             </div>
             <div className="about-page-skills">
                 <h3 className='about-page__subtitle'>My Skills</h3>
-                <div className="about-page-skills-wrap">
-                    
-                </div>
+                <ul className="about-page-skills-items">
+                    {
+                        imgArr.map((item) => {
+                            return (
+                                itemStack(item.name, item.img)
+                            )
+                        })
+                    }
+                </ul>
             </div>
-            <p className='about-page__txt'>I love clean, simple, elegant pages — with no clutter, just thoughtful design and smooth functionality. I believe in the “less is more” philosophy where everything has a purpose and works beautifully.</p>
-            <p className='about-page__txt'>This site marks the beginning of my new path. I’m open to interesting projects and connections with like-minded developers.</p>
-            <div className="social-panel"></div>
+            <div className="about-page-start-box">
+                <div className="about-page-start-box-txt">
+                    <h3 className='about-page__subtitle'>In the end...</h3>
+                    <p className='about-page__txt'>I love clean, simple, elegant pages — with no clutter, just thoughtful design and smooth functionality. I believe in the “less is more” philosophy where everything has a purpose and works beautifully.</p>
+                    <p className='about-page__txt'>This site marks the beginning of my new path. I’m open to interesting projects and connections with like-minded developers.</p>
+                    <div className="social-panel"></div>
+                </div>
+                <img src={page} alt="about-page__img" className="about-page__img" />
+            </div>
         </section>
     );
 };
