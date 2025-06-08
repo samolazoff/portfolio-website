4fdeb41819845bb143e16a9ac3f59cd4d5d013a4
Samolazoff <samolazov.roman.alex@gmail.com>
2025-06-08 15:55:05 +0300

    Add txt in store for about blog

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,3 +1,5 @@
+import {useSelector} from 'react-redux';
+
 import './About.scss';
 
 import page from '/ChatGPT.png';
@@ -16,6 +18,8 @@ import imgMongo from '../../static/icons/mongo-svgrepo-com.svg';
 import imgBoots from '../../static/icons/bootstrap-svgrepo-com.svg';
 import imgTail from '../../static/icons/tailwind-svgrepo-com.svg';
 import imgGit from '../../static/icons/git-svgrepo-com.svg';
+
+import dataTXT from '../../data/language.json';
 
 function itemStack(name, img) {
     return(
@@ -85,19 +89,24 @@ const imgArr = [
 ];
 
 const About = () => {
+
+    const language = useSelector((state) => state.language.language);
+    const txt = (language === 'EN') ? dataTXT.EU.aboutPage : dataTXT.RU.aboutPage;
+    const {title, subtitleMain, txtMain1, txtMain2, subtitleSkills, subtitleEnd, txtEnd1, txtEnd2} = txt;
+
     return (
         <section className='about-page container'>
             <div className="about-page-start-box">
                 <div className="about-page-start-box-txt">
-                    <h2 className='title-block'>About Me</h2>
-                    <h3 className='about-page__subtitle'>Hi there! I'm Roman, web developer.</h3>
-                    <p className='about-page__txt'>I started building websites back in 2012, just for fun. It was a hobby at first — learning HTML, writing scripts late at night, experimenting with layouts. Today, I’ve decided to make it my main profession. For me, web development is more than just code — it’s a way to create something real and meaningful.</p>
-                    <p className='about-page__txt'>My main stack is MERN (MongoDB, Express, React, Node.js), but I’m always learning and growing. Lately, I’ve been diving into Next.js and exploring PHP. Honestly, I’m curious about everything — backend, frontend, UI, performance — the whole journey excites me.</p>
+                    <h2 className='title-block'>{title}</h2>
+                    <h3 className='about-page__subtitle'>{subtitleMain}</h3>
+                    <p className='about-page__txt'>{txtMain1}</p>
+                    <p className='about-page__txt'>{txtMain2}</p>
                 </div>
                 <img src={page} alt="about-page__img" className="about-page__img" />
             </div>
             <div className="about-page-skills">
-                <h3 className='about-page__subtitle'>My Skills</h3>
+                <h3 className='about-page__subtitle'>{subtitleSkills}</h3>
                 <ul className="about-page-skills-items">
                     {
                         imgArr.map((item) => {
@@ -110,9 +119,9 @@ const About = () => {
             </div>
             <div className="about-page-start-box">
                 <div className="about-page-start-box-txt">
-                    <h3 className='about-page__subtitle'>In the end...</h3>
-                    <p className='about-page__txt'>I love clean, simple, elegant pages — with no clutter, just thoughtful design and smooth functionality. I believe in the “less is more” philosophy where everything has a purpose and works beautifully.</p>
-                    <p className='about-page__txt'>This site marks the beginning of my new path. I’m open to interesting projects and connections with like-minded developers.</p>
+                    <h3 className='about-page__subtitle'>{subtitleEnd}</h3>
+                    <p className='about-page__txt'>{txtEnd1}</p>
+                    <p className='about-page__txt'>{txtEnd2}</p>
                     <div className="social-panel"></div>
                 </div>
                 <img src={page} alt="about-page__img" className="about-page__img" />
