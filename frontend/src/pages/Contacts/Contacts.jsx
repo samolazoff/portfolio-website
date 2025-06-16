09bf6be8c51e15d63e4f4a69b2ff78d98a3177a8
Samolazoff <samolazov.roman.alex@gmail.com>
2025-06-16 06:53:11 +0300

    Create page contacts

--- frontend/src/pages/Contacts/Contacts.jsx
+++ frontend/src/pages/Contacts/Contacts.jsx
@@ -1,12 +1,44 @@
-import React from 'react';
-
 import './Contacts.scss';
+
+import SocialMedia from '../../components/SocialMedia/SocialMedia';
 
 const Contacts = () => {
     return (
-        <div>
-            Contacts
-        </div>
+        <section className = 'page-contacts container'>
+            <h2 className="title-block">Contacts</h2>
+            <div className="page-contacts-wrap">
+                <div className="contacts-box-txt">
+                    <SocialMedia/>
+                </div>
+                <div className="contacts-box">
+                    <h3 className="contacts-title">Contact Me</h3>
+                    <p className="contacts-subtitle">
+                    Have a project or a question? Let’s get in touch.
+                    </p>
+                    <form className="contacts-form">
+                    <label>
+                        Your Name
+                        <input type="text" name="name" placeholder="John Doe" required />
+                    </label>
+
+                    <label>
+                        Email Address
+                        <input type="email" name="email" placeholder="you@example.com" required />
+                    </label>
+
+                    <label>
+                        Your Message
+                        <textarea name="message" rows="4" placeholder="Type your message..." required></textarea>
+                    </label>
+
+                    <button type="submit" className="contacts-button">Send Message</button>
+                    </form>
+                    <p className="contacts-email">
+                    Or email me at <a href="mailto:samolazov.roman.alex@gmail.com">samolazov.roman.alex@gmail.com</a>
+                    </p>
+                </div>
+            </div>
+        </section>
     );
 };
 
