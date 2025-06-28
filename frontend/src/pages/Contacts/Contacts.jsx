bf87c81e94472b878be17ffc4fa5587f8c2fc913
Samolazoff <samolazov.roman.alex@gmail.com>
2025-06-28 14:42:00 +0300

    Chage block contact

--- frontend/src/pages/Contacts/Contacts.jsx
+++ frontend/src/pages/Contacts/Contacts.jsx
@@ -6,9 +6,34 @@ import ContactMe from '../../components/ContactMe/ContactMe';
 const Contacts = () => {
     return (
         <section className = 'page-contacts container'>
-            <h2 className="title-block">Contacts</h2>
-            <div className="page-contacts-wrap">
-                <div className="contacts-box-txt">
+            <h2 className = "title-block" >Contacts</h2>
+            <div className = "page-contacts-wrap">
+                <div className = "contacts-box-txt">
+                    <table>
+                        <caption>My contacts</caption>
+                        <tbody>
+                            <tr>
+                                <td className = 'main-txt'>email</td>
+                                <td>samolazov.roman.alex@gmail.com</td>
+                            </tr>
+                            <tr>
+                                 <td className = 'main-txt'>mobile</td>
+                                <td>+375445576259</td>
+                            </tr>
+                            <tr>
+                                 <td className = 'main-txt'>telegram</td>
+                                <td>+375445576259</td>
+                            </tr>
+                            <tr>
+                                <td className = 'main-txt'>watsapp</td>
+                                <td>+375445576259</td>
+                            </tr>
+                            <tr>
+                                <td className = 'main-txt'> viber</td>
+                                <td>+375445576259</td>
+                            </tr>
+                        </tbody>
+                    </table>
                     <SocialMedia/>
                 </div>
                 <ContactMe/>
